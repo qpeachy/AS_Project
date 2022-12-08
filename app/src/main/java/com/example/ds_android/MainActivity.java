bcbd7489d17cb89b0d5db35b597752e29e8a5cdc
mathy <mathypalelo@gmail.com>
2022-12-08 11:40:13 +0100

    try

--- app/src/main/java/com/example/ds_android/MainActivity.java
+++ app/src/main/java/com/example/ds_android/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.ds_android;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.ds_android.Instance.RetrofitClientInstance;
@@ -10,6 +17,7 @@ import com.example.ds_android.Model.Forecast;
 import com.example.ds_android.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -18,6 +26,20 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private List<Forecast> dataForecast;
+    private RecyclerViewAdapteForecasts adapter;
+    private ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if(result.getResultCode()==1){
+                Intent resultIntent = result.getData();
+                if(resultIntent != null){
+                    Forecast nForecast = (Forecast) resultIntent.getSerializableExtra("Forecast");
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +47,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.ListForecast.setHasFixedSize(true);
+        LinearLayoutManager LayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        binding .ListForecast.setLayoutManager(LayoutManager);
+        binding.ListForecast.setFocusable(false);
 
         OpenWeatherService service =
                 RetrofitClientInstance.getRetrofitInstance().create(OpenWeatherService. class);
@@ -32,7 +58,11 @@ public class MainActivity extends AppCompatActivity {
         call.enqueue(new Callback<Forecast>() {
             @Override
             public void onResponse(Call<Forecast> call, Response<Forecast> response) {
-                Forecast forecast = response.body();
+                Forecast forecasts = response.body();
+                dataForecast = new ArrayList<>();
+                dataForecast = (List<Forecast>) forecasts;
+                adapter = new RecyclerViewAdapteForecasts((ArrayList<Forecast>) dataForecast);
+                binding.ListForecast.setAdapter(adapter);
             }
             @Override
             public void onFailure(Call<Forecast> call, Throwable t) {  Toast.makeText(MainActivity.this, "Une erreur est survenue !",  Toast.LENGTH_SHORT).show();
