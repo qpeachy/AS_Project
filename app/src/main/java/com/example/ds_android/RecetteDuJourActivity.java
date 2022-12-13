84700960c230c7ebb0b58951c797753fdc8a6254
mathy <mathypalelo@gmail.com>
2022-12-13 08:19:47 +0100

    mod

--- app/src/main/java/com/example/ds_android/RecetteDuJourActivity.java
+++ app/src/main/java/com/example/ds_android/RecetteDuJourActivity.java
@@ -9,6 +9,6 @@ public class RecetteDuJourActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recette_du_jour);
+        setContentView(R.layout.activity_recette);
     }
 }
